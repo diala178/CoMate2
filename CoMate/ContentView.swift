8011a03b7d4095f0553ca7abed2de634522d6f92
DialaMasoud <diala.masoud123@hotmail.com>
2023-10-08 17:43:20 +0300

    adding 5:43

--- CoMate/ContentView.swift
+++ CoMate/ContentView.swift
@@ -8,24 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    var h: Int = 0
-    var body: some View {
-        VStack {
-            Text("Water Tracker ")
-            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
-                Text("Apple health")
-            }
-            
-            Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Range@*/1...10/*@END_MENU_TOKEN@*/) {
-                Text("Cups to drink per day 0")
-            }
-
-            
-            
     
-            Button("Continue") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+    
+    
+    var body: some View {
+        
+        VStack {
+            ZStack {
+                
+                Image("diala")
+                    .resizable()
+                    .frame(width:300, height : 300)
+                    .cornerRadius(150)
+                
+                
             }
+            .padding(/*@START_MENU_TOKEN@*/EdgeInsets()/*@END_MENU_TOKEN@*/)
+            
+                
+                
+            Text("Diala Masoud")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .bold()
+                .foregroundColor(.black)
+            Text("Hello, I am highly motivated to learn about swift ")
+                .padding()
+            
+            
+            
         }
         .padding()
     }
@@ -34,3 +44,8 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+
+
+ 
