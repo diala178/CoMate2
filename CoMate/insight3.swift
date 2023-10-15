7e4bcf1e81842d549bf6cf2c5084056ac2cbd4bb
NorahAldughem <naldughem23@twq.idserve.net>
2023-10-15 15:11:38 +0300

    insight3

---
+++ CoMate/insight3.swift
@@ -0,0 +1,18 @@
+//
+//  insight3.swift
+//  CoMate
+//
+//  Created by Norah Aldughem on 30/03/1445 AH.
+//
+
+import SwiftUI
+
+struct insight3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    insight3()
+}
