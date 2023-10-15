c3ced7e99a2d6c21ce75ba069e9f459766347e4b
NorahAldughem <naldughem23@twq.idserve.net>
2023-10-15 15:12:43 +0300

    isight3333

--- CoMate/insight3.swift
+++ CoMate/insight3.swift
@@ -5,14 +5,57 @@
 //  Created by Norah Aldughem on 30/03/1445 AH.
 //
 
+
 import SwiftUI
 
-struct insight3: View {
+struct Insight3: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ZStack {
+                Color.base
+                    .ignoresSafeArea()
+                Text("Insight")
+                    .bold()
+                    .font(.largeTitle)
+                    .padding(.leading,-175)
+                    .padding(.top,-350)
+                ZStack{
+                    RoundedRectangle(cornerRadius: 12.0)
+                        .frame(width: 300, height: 500)
+                        .foregroundColor(.white)
+                    Text("Sorry 😞")
+                        .bold()
+                        .font(.title2)
+                        .padding(.top,-200)
+                    Image("insight3")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 300)
+                    Text("You need to work more on")
+                        .font(.title3)
+                        .multilineTextAlignment(.center)
+                        .padding(.top,320)
+                    
+                    Text("your communication skills")
+                        .font(.title3)
+                        .multilineTextAlignment(.center)
+                        .padding(.top,375)
+                    NavigationLink(destination:Quiz_Page() ) {
+                        Text("Start Again")
+                            .foregroundColor(.white)
+                            .frame(width: 91, height: 50)
+                            .background(Color.button)
+                            .cornerRadius(10)
+                            .padding(.top,650)
+                    }
+                }
+             
+            }
+            
+        }
     }
 }
 
 #Preview {
-    insight3()
+    Insight3()
 }
