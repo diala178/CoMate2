08aeafa220dc5f31f38c78b32a8a55d47222243f
remalwneen <remalwneen@icloud.com>
2023-10-15 15:21:59 +0300

    insight2

---
+++ CoMate/insight2.swift
@@ -0,0 +1,18 @@
+//
+//  insight2.swift
+//  CoMate
+//
+//  Created by Rimah Saleh Alwanin on 15/10/2023.
+//
+
+import SwiftUI
+
+struct insight2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    insight2()
+}
